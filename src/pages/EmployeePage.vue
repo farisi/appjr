a6053b8e838e313fa3100ea018eb07856d43bc92
salman <salmandriva@gmail.com>
2024-03-19 15:23:29 +0700

    coba menambahkan untuk bisa edit

--- src/pages/EmployeePage.vue
+++ src/pages/EmployeePage.vue
@@ -7,13 +7,20 @@
         <div class="card">
             <h5>Employyes List</h5>
 
-            <DataTable :value="employees" :paginator="true" :rows="10" stripedRows removableSort>
+            <DataTable v-model:editingRows="editingRows" :value="employees" :paginator="true" :rows="10" stripedRows removableSort editMode="row" dataKey="id" >
                 <Column  header="No" style="min-width: 5rem" >
                 <template #body="{ index,data }">
                     {{ index + 1}}
                 </template>
                 </Column>
-                <Column field="firstName" header="First Name" sortable  style="min-width:10%" ></Column>
+                <Column field="firstName" header="First Name" sortable  style="min-width:10%" >
+                    <template #body="{ data, field }">
+                        {{data[field]}}
+                    </template>
+                    <template #editor="{ data, field }">
+                        <InputText v-model="data[field]"  />
+                    </template>
+                </Column>
                 <Column field="lastName" header="Last Name" sortable style="min-width:10%" ></Column>
                 <Column field="email" header="Email" sortable style="width: 10%"></Column>
                 <Column field="mobile" header="Mobile" sortable style="width: 10%"></Column>
@@ -29,12 +36,14 @@
                         {{ formatDateTime(data.joinDate)}}
                     </template>
                 </Column>
+                <!--
                 <Column header="Action"  style="width: 15%">
                     <template #body="{ data }">
                         <Button icon="pi pi-file-edit" severity="warning" aria-label="Submit" />
                         <Button icon="pi pi-trash" severity="danger" />
                     </template>
-                </Column>
+                </Column>-->
+                <Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
             </DataTable>
         </div>
     </div>
@@ -48,10 +57,12 @@ import InputIcon from 'primevue/inputicon';
 import Column from 'primevue/column';
 import Button from 'primevue/button';
 import { storeToRefs } from 'pinia'
+import InputText from 'primevue/inputtext';
 import { useEmployeeStore } from '@/stores/EmployeeStore';
 
 const { employees, loading, error } = storeToRefs(useEmployeeStore())
 const {fetchEmployees} = useEmployeeStore()
+const editingRows = ref([]);
 fetchEmployees();
 
 const formatDate=(data)=>{
