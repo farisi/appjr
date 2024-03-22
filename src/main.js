32a6f4a6425ca9b77dc5c3720ba644c0b60d6be3
salman <salmandriva@gmail.com>
2024-03-22 16:38:20 +0700

    mengatur style untuk navbar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import   './style.css'
 import 'primevue/resources/themes/aura-light-blue/theme.css'
 import 'primeicons/primeicons.css'
 import App from "./App.vue";
